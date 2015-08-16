b9cca96828e2fd3a3a6bff3a0f3ae8f6dc4bbfad
ColeHud <cmhudson11@gmail.com>
2015-08-16 07:31:01 -0700

    Login works, user must have enough money

--- SpriteKitSimpleGame/SpriteKitSimpleGame/PaymentHandlerViewController.swift
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/PaymentHandlerViewController.swift
@@ -30,6 +30,9 @@ class PaymentHandlerViewController: UIViewController
     var password = ""
     var link = ""
     
+    var bitcoinInTwentyFiveCents = 0.0
+    var userHasEnoughMoney = false
+    
     //dismiss the keyboard
     func dismissKeyboard()
     {
@@ -79,6 +82,9 @@ class PaymentHandlerViewController: UIViewController
     {
         super.viewDidLoad()
         
+        //get the number of bitcoin in a quarter
+        self.twentyFiveCentsInBitcoin()
+        
         //set up keyboard dismiss
         var touchDismiss: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         touchDismiss.numberOfTapsRequired = 1
@@ -91,6 +97,9 @@ class PaymentHandlerViewController: UIViewController
         backButton.layer.borderColor = UIColor.blackColor().CGColor
         
         self.valuesStored()
+        
+        //does the user have enough???
+        doesUserHaveEnoughMoneyToPlayGame()
         
     }
 
@@ -157,7 +166,8 @@ class PaymentHandlerViewController: UIViewController
         self.valuesStored()
     }
     
-    func twentyFiveCentsInBitcoin() -> Double
+    //25 cents converted to bitcoin
+    func twentyFiveCentsInBitcoin()
     {
         let url = NSURL(string: "https://blockchain.info/tobtc?currency=USD&value=.25&api_code=dbcaa55e-9fa1-48e1-aa9d-4d28814ceda8")!
         let request = NSMutableURLRequest(URL: url)
@@ -167,28 +177,22 @@ class PaymentHandlerViewController: UIViewController
             
             if error != nil {
                 // Handle error...
+                println(error)
                 return
             }
             
-            println(error)
-            println(response)
-            println(NSString(data: data, encoding: NSUTF8StringEncoding))
-            
-            var error = NSErrorPointer()
-            var json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: error)
-            
-            let twentyFiveCentsInBitcoin = json!.objectForKey("") as! Double
-            println("25 cents is equal to \(twentyFiveCentsInBitcoin) bitcoin")
+            //println(response)
+            var stringy = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println(stringy)
+            self.bitcoinInTwentyFiveCents = stringy!.doubleValue
         }
-        
-        return 1.0
         task.resume()
     }
     
-    func isPositiveBalance() -> Bool
+    //is the user's balance greater than 25 cents?
+    func doesUserHaveEnoughMoneyToPlayGame()
     {
         //check and see if the user has any funds
-        var balanceIsGreaterThanZero = false
         let url = NSURL(string: "https://blockchain.info/merchant/\(guid)/balance?password=\(password)&api_code=dbcaa55e-9fa1-48e1-aa9d-4d28814ceda8")!
         let request = NSMutableURLRequest(URL: url)
         
@@ -200,8 +204,8 @@ class PaymentHandlerViewController: UIViewController
                 return
             }
             
-            println(error)
-            println(response)
+            //println(error)
+            //println(response)
             println(NSString(data: data, encoding: NSUTF8StringEncoding))
             
             var error = NSErrorPointer()
@@ -209,22 +213,22 @@ class PaymentHandlerViewController: UIViewController
             
             let balance = json!.objectForKey("balance") as! Double
             
-            if(balance > 0)
+            println("balance: \(balance/100000000), 25 cents = \(self.bitcoinInTwentyFiveCents)")
+            if(balance/100000000 > self.bitcoinInTwentyFiveCents)
             {
-                balanceIsGreaterThanZero = true
+                println("User has enough money to play a game")
+                self.userHasEnoughMoney = true
             }
         }
         
         task.resume()
-        return balanceIsGreaterThanZero
     }
     
     
     //pay 25 cents and start the game
     @IBAction func payTwentyFiveCents(sender: AnyObject)
     {
-        self.twentyFiveCentsInBitcoin()
-        
+        doesUserHaveEnoughMoneyToPlayGame()
         //the user hasn't signed in yet
         if(self.guid == "" || guid.length < 2)
         {
@@ -242,7 +246,7 @@ class PaymentHandlerViewController: UIViewController
         {
             
             
-            if(isPositiveBalance() == false)
+            if(self.userHasEnoughMoney == false)
             {
                 //alert the user
                 var alert = UIAlertView(title: "$$$", message: "You don't have enough money to play", delegate: nil, cancelButtonTitle: "ok")
