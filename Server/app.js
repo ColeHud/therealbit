34f7e436785a31f8f2e8a7df787bf2be95e1c999
ColeHud <cmhudson11@gmail.com>
2015-08-16 09:51:31 -0700

    Finalized for presentation at Hack the Planet

--- Server/app.js
+++ Server/app.js
@@ -5,7 +5,6 @@ var Parse = require('parse').Parse;
 
 //set up parse
 Parse.initialize("T7IdleWlnGMMAnwREm9clA8UogD46ZOIKnDq4t8Y", "7j1Dckyuv2QIiKbwgIsnSijfVnhnh5KspMBTkdnl");
-var app = new Parse(options);
 
 //get the address of the person with the most points
 var getWinnerAddress = function(cb)
@@ -15,10 +14,10 @@ var getWinnerAddress = function(cb)
 	query.equalTo("objectId", "XVKrk2qZjw");
 	query.find({
   		success: function(results) {
-   		 	alert("Successfully retrieved " + results.length + " scores");
+   		 	console.log("Successfully retrieved " + results.length + " scores");
     		// Do something with the returned Parse.Object values
       			var object = results[0];
-      			cb(null, object.get('address')));
+      			cb(null, object.get('address'));
   			},
   			error: function(error) {
   	  		alert("Error: " + error.code + " " + error.message);
@@ -31,12 +30,16 @@ var getWinnerAddress = function(cb)
 var payTheWinner = function(address)
 {
 	var myWallet = new blockchain.MyWallet("f0c25bc8-3a69-4730-8c8e-4a4e4b132339", "$Hudson12compsci");
-	myWallet.getBalance([false], function(err, data){
+	myWallet.getBalance(function(err, data){
 		var options = {
 			'to': address,
-			'amount': wallet.balance,
+			'amount': data,
 			'from': '1NuL6cSsndGRCEk9dijAa9v7ysqo4qQax5',
-			'fee': 100
+			'fee': 1000
+		}
+		if(err)
+		{
+			console.log(err);
 		}
 		myWallet.send(options, function(err, data){
 			console.log("Winner Paid");
@@ -48,8 +51,8 @@ var payTheWinner = function(address)
 var rule = new schedule.RecurrenceRule();
 
 rule.dayOfWeek = [0, new schedule.Range(1, 6)];
-rule.hour = 23;
-rule.minute = 59;
+rule.hour = 9;
+rule.minute = 5;
 
 var j = schedule.scheduleJob(rule, function(){
 	getWinnerAddress(function(err, address) {
