d4af789087b75b1b9bc04eeceeec798bab283f58
ColeHud <cmhudson11@gmail.com>
2015-08-15 22:07:18 -0700

    Created basic UI and class structure for blockchain login

--- SpriteKitSimpleGame/SpriteKitSimpleGame/PaymentHandlerViewController.swift
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/PaymentHandlerViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class PaymentHandlerViewController: UIViewController
 {
+    //views
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var emailAndAddress: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var loginAndSignup: UIButton!
+    @IBOutlet var twentyFiveCentButton: UIButton!
+    
+    //dismiss the keyboard
+    func dismissKeyboard()
+    {
+        print("\nDismissing keyboard")
+        passwordField.resignFirstResponder()
+        emailAndAddress.resignFirstResponder()
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        //set up keyboard dismiss
+        var touchDismiss: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        touchDismiss.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(touchDismiss)
         
     }
 }
